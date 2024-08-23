537bbbc15ea546c23bd1b4e6ac321427472ad7ef
TripleBladeOffic <TripleBladeOfficial@gmail.com>
2024-08-23 16:05:49 +0500

    Create register.php

---
+++ php/register.php
@@ -0,0 +1,30 @@
+<?php
+// Connect to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "my_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Registration logic
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+$conn->close();
+?>
